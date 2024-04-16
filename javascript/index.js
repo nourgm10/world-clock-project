913ec282add0b3f492081611d611c32c35daa038
nourgm10 <nourgm10@gmail.com>
2024-04-16 10:17:02 +0200

    fixed JS and updated CSS

--- javascript/index.js
+++ javascript/index.js
@@ -1,16 +1,42 @@
-function updateTime() {
-  let cityElement = document.querySelector(".city-name");
+function updateTime(cityTimeZone) {
   let dateElement = document.querySelector(".date");
   let timeElement = document.querySelector(".time");
-  let currentTime = moment().tz("Europe/Paris");
 
-  cityElement.innerHTML = `Paris`;
+  if (!cityTimeZone) {
+    // If cityTimeZone is an empty string, use the guessed timezone
+    cityTimeZone = moment.tz.guess();
+  }
+
+  let currentTime = moment().tz(cityTimeZone);
+
   dateElement.innerHTML = currentTime.format("MMMM Do, YYYY");
   timeElement.innerHTML = currentTime.format("h:mm:ss [<small>] A[</small>]");
 }
 
-// let selectElement = document.querySelector("#choose-city");
-// selectElement.addEventListener("select", handleSearchSubmit);
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (!cityTimeZone) {
+    return;
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#city-block");
+  citiesElement.innerHTML = `<div class="city">
+          <span class="city-name">${cityName}</span>
+          <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss [<small>] A[</small>]"
+        )}</div>`;
+  updateTime(cityTimeZone);
+}
 
-updateTime();
-setInterval(updateTime, 1000);
+updateCity({ target: { value: moment.tz.guess() } });
+
+let selectElement = document.querySelector("#choose-city");
+selectElement.addEventListener("change", updateCity);
+
+setInterval(function () {
+  let selectedCity = document.querySelector("#choose-city").value;
+  updateTime(selectedCity);
+}, 1000);
